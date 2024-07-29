69c1a0a957d8faeec7073c34640e1ab0f53d6d06
robpSF <robert@tstoryteller.com>
2024-07-29 19:44:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,6 +214,6 @@ if persona_details and social_graph:
         g.save_graph('networkviz.html')
         HtmlFile = open("networkviz.html", 'r', encoding='utf-8')
         source_code = HtmlFile.read()
-        st.components.v1.html(source_code, height=1000, width="100%")
+        st.components.v1.html(source_code, height=1000, width=1000)
 
     st.header("All Done!")
