5e177d7b88e4fa6240abb66dbd59a69b88dd2682
robpSF <robert@tstoryteller.com>
2024-07-29 21:32:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,32 @@ NAMES_ROW = 0
 HANDLES_COL = 2  # Remember columns start numbering at 0 in Python
 FACTIONS_COL = 3
 
+# Configurable default affinity
+DEFAULT_AFFINITY = 0.1  # Default value to use if an affinity is missing
+
 # Start to create the network viz
-g = net.Network(height='1025px', width='50%', heading='', directed=True)
+g = net.Network(height='1000px', width='100%', bgcolor='#222222', font_color='white', directed=True)
 g.set_options('''
 var options = {
     "nodes": {
         "borderWidth": 1,
-        "borderWidthSelected": 8
+        "borderWidthSelected": 8,
+        "color": {
+            "highlight": {
+                "background": "red",
+                "border": "yellow"
+            }
+        }
     },
     "edges": {
-        "borderWidthSelected": 8
+        "color": {
+            "color": "lightgray",
+            "highlight": "yellow",
+            "hover": "lightblue"
+        },
+        "width": 2,
+        "hoverWidth": 3,
+        "selectionWidth": 3
     },
     "physics": {
         "barnesHut": {
@@ -42,11 +58,9 @@ var options = {
 def whats_the_friendship(a, b, attraction_df, affinity_df):
     # Get the persona factions
     if a not in attraction_df.TwHandle.values:
-        st.write(f"Handle {a} not found in attraction_df")
         return 0, 0
 
     if b not in attraction_df.TwHandle.values:
-        st.write(f"Handle {b} not found in attraction_df")
         return 0, 0
 
     faction_a = attraction_df.loc[attraction_df.TwHandle == a, "Faction"].values[0]
@@ -54,52 +68,45 @@ def whats_the_friendship(a, b, attraction_df, affinity_df):
     try:
         affinity_between_factions = affinity_df.loc[(affinity_df.Faction == faction_a) & (affinity_df.Other_Faction == faction_b), "Affinity"].values[0]
     except:
-        affinity_between_factions = 0
+        affinity_between_factions = DEFAULT_AFFINITY  # Use default affinity if not found
+        st.write(f"Affinity between {faction_a} and {faction_b} not found. Using default affinity {DEFAULT_AFFINITY}.")
 
     # FIRST pass "a is followed by b?"
-    if faction_a == faction_b:  # Need to use the "inter-faction affinity"
+    if faction_a == faction_b:  # Intra-faction probability
         likelihood_of_following = attraction_df.loc[attraction_df.TwHandle == a, "Prob4Faction"].values[0]
         affinity_between_factions = 1  # This overrides the "0" from above
-        # Small fiddle
-        # I found that in small samples the inter-faction wasn't high enough for my liking :)
-        # Therefore I'm bumping up the likelihood to get chance of more followers
+        # Adjust likelihood based on follower count
         followers = attraction_df.loc[attraction_df.TwHandle == a, "TwFollowers"].values[0]
         if followers > 5000 and followers < 10000:
-            st.write(a)
-            st.write(likelihood_of_following)
             likelihood_of_following = likelihood_of_following + 0.2
         if followers < 1000:
             likelihood_of_following = likelihood_of_following - 0.1
     else:
-        # If they're different factions then the likelihood of following is based on the persona's reach * affinity between the factions (i.e. reduced if unlikely)
+        # Inter-faction probability
         likelihood_of_following = attraction_df.loc[attraction_df.TwHandle == a, "ProbOverAll"].values[0] * affinity_between_factions
 
     dice_roll = randrange(100) / 100
-    if likelihood_of_following > dice_roll:  # Then b is following a
+    if likelihood_of_following > dice_roll:  # If likelihood is greater than dice roll
         x = 3
     else:
         x = 0
 
     # SECOND pass "b is followed by a?"
-    if faction_a == faction_b:  # Need to use the "inter-faction affinity"
+    if faction_a == faction_b:  # Intra-faction probability
         likelihood_of_following = attraction_df.loc[attraction_df.TwHandle == b, "Prob4Faction"].values[0]
         affinity_between_factions = 1  # This overrides the "0" from above
-        # Small fiddle
-        # I found that in small samples the inter-faction wasn't high enough for my liking :)
-        # Therefore I'm bumping up the likelihood to get chance of more followers
+        # Adjust likelihood based on follower count
         followers = attraction_df.loc[attraction_df.TwHandle == b, "TwFollowers"].values[0]
         if followers > 5000 and followers < 10000:
-            st.write(b)
-            st.write(likelihood_of_following)
             likelihood_of_following = likelihood_of_following + 0.2
         if followers < 1000:
             likelihood_of_following = likelihood_of_following - 0.1
     else:
-        # If they're different factions then the likelihood of following is based on the persona's reach * affinity between the factions (i.e. reduced if unlikely)
+        # Inter-faction probability
         likelihood_of_following = attraction_df.loc[attraction_df.TwHandle == b, "ProbOverAll"].values[0] * affinity_between_factions
 
     dice_roll = randrange(100) / 100
-    if likelihood_of_following > dice_roll:  # Then b is following a
+    if likelihood_of_following > dice_roll:  # If likelihood is greater than dice roll
         y = 1
     else:
         y = 0
@@ -133,11 +140,14 @@ if persona_details and social_graph:
     max_rows = social_graph_sheet.max_row
 
     handles = [social_graph_sheet.cell(row=i, column=HANDLES_COL + 1).value for i in range(2, max_rows + 1)]
+    handles = [handle for handle in handles if handle]  # Remove None values
 
     factions = [social_graph_sheet.cell(row=i, column=FACTIONS_COL + 1).value for i in range(2, max_rows + 1)]
     factions = list(dict.fromkeys(factions))
-    factions.remove("Faction")
-    #factions.remove("")
+    if "Faction" in factions:
+        factions.remove("Faction")
+    if "" in factions:
+        factions.remove("")
     st.write(factions)
 
     factions_df = pd.DataFrame(factions, columns=["Faction"])
@@ -152,8 +162,18 @@ if persona_details and social_graph:
         affinity_df = affinity_df[["Faction", "Other_Faction", "Affinity"]]
         st.table(affinity_df)
 
-        for i in range(1, max_rows):
-            persona = handles[i - 1]
+        # Progress bar
+        progress_text = st.empty()
+        progress_bar = st.progress(0)
+
+        total_steps = len(handles) + (len(handles) * (len(handles) - 1)) // 2
+        step = 0
+
+        # Add nodes to the graph
+        for i in range(len(handles)):
+            persona = handles[i]
+            if persona not in attraction_df.TwHandle.values:
+                continue
             bio = df2.loc[df2.TwHandle == persona, "TwBio"].values[0] if not df2.loc[df2.TwHandle == persona, "TwBio"].empty else ""
             faction = df2.loc[df2.TwHandle == persona, "Faction"].values[0] if not df2.loc[df2.TwHandle == persona, "Faction"].empty else ""
 
@@ -162,19 +182,33 @@ if persona_details and social_graph:
             except:
                 g.add_node(persona, title="(" + persona + ")[" + faction + "] ")
 
-        for i in range(2, len(handles) + 1):
-            for j in range(i + 1, len(handles) + 1):
-                followed = handles[i - 1]
-                follower = handles[j - 1]
+            step += 1
+            progress_percentage = step / total_steps
+            progress_text.text(f"Processing nodes: {step} / {len(handles)}")
+            progress_bar.progress(progress_percentage)
 
-                friend_value_x, friend_value_y = whats_the_friendship(followed, follower, attraction_df, affinity_df)
+        # Add edges to the graph
+        for i in range(len(handles)):
+            for j in range(i + 1, len(handles)):
+                followed = handles[i]
+                follower = handles[j]
 
-                if friend_value_x > 0:
-                    g.add_edge(follower, followed)
-                    social_graph_sheet.cell(row=i, column=j + 2, value=friend_value_x)
-                elif friend_value_y > 0:
-                    g.add_edge(followed, follower)
-                    social_graph_sheet.cell(row=i, column=j + 2, value=friend_value_y)
+                try:
+                    friend_value_x, friend_value_y = whats_the_friendship(followed, follower, attraction_df, affinity_df)
+
+                    if friend_value_x > 0:
+                        g.add_edge(follower, followed)
+                        social_graph_sheet.cell(row=i + 2, column=j + 4, value=friend_value_x)  # Shifted down and right
+                    elif friend_value_y > 0:
+                        g.add_edge(followed, follower)
+                        social_graph_sheet.cell(row=i + 2, column=j + 4, value=friend_value_y)  # Shifted down and right
+                except Exception as e:
+                    st.write(f"Error processing friendship between {followed} and {follower}: {e}")
+
+                step += 1
+                progress_percentage = step / total_steps
+                progress_text.text(f"Processing edges: {step - len(handles)} / {(len(handles) * (len(handles) - 1)) // 2}")
+                progress_bar.progress(progress_percentage)
 
         output_path = 'social_OUTPUT.xlsx'
         source_wb.save(output_path)
